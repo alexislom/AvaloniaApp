180da65a5ad512a910e40344733550c7061d5bcd
Aliaksei Lamaka <alexislomako@gmail.com>
2025-10-30 00:45:03 +0300

    Change OxyPlot to LiveCharts2

--- AvaloniaApp/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApp/ViewModels/MainWindowViewModel.cs
@@ -1,217 +1,98 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.IO;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reactive;
-using System.Text.Json;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Platform.Storage;
-using OxyPlot;
-using OxyPlot.Axes;
+using System.Windows.Input;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.Painting.Effects;
 using ReactiveUI;
-using AvaloniaApp.Models; // Namespace, где FunctionModel и Node
+using SkiaSharp;
 
-namespace AvaloniaApp.ViewModels
+namespace AvaloniaApp.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
 {
-    public class MainWindowViewModel : ReactiveObject
-    {
-        // --- Свойства -------------------------------------------------------
-        private FunctionModel? _selectedFunction;
-        private Node? _draggedNode;
-        private bool _isDirty;
+    public ObservableCollection<ObservablePoint> Points { get; set; }
+    public ISeries[] Series { get; set; }
 
-        public ObservableCollection<FunctionModel> Functions { get; } = new();
-        public FunctionModel? SelectedFunction
+    public Axis[] XAxes { get; set; } =
+    [
+        new()
         {
-            get => _selectedFunction;
-            set => this.RaiseAndSetIfChanged(ref _selectedFunction, value);
+            Name = "X Axis",
+            NamePaint = new SolidColorPaint(SKColors.Black),
+            LabelsPaint = new SolidColorPaint(SKColors.Blue),
+            TextSize = 10,
+            SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) { StrokeThickness = 2 }
         }
+    ];
 
-        public bool IsDirty
+    public Axis[] YAxes { get; set; } =
+    [
+        new()
         {
-            get => _isDirty;
-            set => this.RaiseAndSetIfChanged(ref _isDirty, value);
-        }
-
-        // --- Команды --------------------------------------------------------
-        public ReactiveCommand<Unit, Unit> AddFunctionCommand { get; }
-        public ReactiveCommand<Unit, Unit> RemoveFunctionCommand { get; }
-        public ReactiveCommand<Unit, Unit> AddPointCommand { get; }
-        public ReactiveCommand<Node?, Unit> RemovePointCommand { get; }
-        public ReactiveCommand<Unit, Unit> SaveCommand { get; }
-        public ReactiveCommand<Unit, Unit> LoadCommand { get; }
-
-        // --- Конструктор ----------------------------------------------------
-        public MainWindowViewModel()
-        {
-            AddFunctionCommand = ReactiveCommand.Create(AddFunction);
-            RemoveFunctionCommand = ReactiveCommand.Create(RemoveFunction);
-            AddPointCommand = ReactiveCommand.Create(AddPoint);
-            RemovePointCommand = ReactiveCommand.Create<Node?>(RemovePoint);
-            SaveCommand = ReactiveCommand.CreateFromTask(async () => await SaveAsync());
-            LoadCommand = ReactiveCommand.CreateFromTask(async () => await LoadAsync());
-
-            // Создаем пример по умолчанию
-            var f = new FunctionModel { Name = "f(x)" };
-            f.Nodes.Add(new Node{X = 0, Y = 0});
-            f.Nodes.Add(new Node{X = 1, Y = 1});
-            f.Nodes.Add(new Node{X = 2, Y = 0.5});
-            f.RebuildPlot();
-            Functions.Add(f);
-            SelectedFunction = f;
-
-            foreach (var func in Functions)
-                SubscribeToFunction(func);
-        }
-
-        // --- Методы управления функциями -----------------------------------
-        private void AddFunction()
-        {
-            var f = new FunctionModel { Name = $"f{Functions.Count + 1}(x)" };
-            f.Nodes.Add(new Node{X = 0, Y =0});
-            f.Nodes.Add(new Node{X =1,Y = 1});
-            f.RebuildPlot();
-            Functions.Add(f);
-            SelectedFunction = f;
-            SubscribeToFunction(f);
-            IsDirty = true;
-        }
-
-        private void RemoveFunction()
-        {
-            if (SelectedFunction == null) return;
-            Functions.Remove(SelectedFunction);
-            SelectedFunction = Functions.FirstOrDefault();
-            IsDirty = true;
-        }
-
-        // --- Методы работы с узлами ----------------------------------------
-        private void AddPoint()
-        {
-            if (SelectedFunction == null) return;
-            SelectedFunction.Nodes.Add(new Node{X =0, Y =0});
-            SelectedFunction.RebuildPlot();
-            IsDirty = true;
-        }
-
-        private void RemovePoint(Node? node)
-        {
-            if (node == null || SelectedFunction == null) return;
-            SelectedFunction.Nodes.Remove(node);
-            SelectedFunction.RebuildPlot();
-            IsDirty = true;
-        }
-
-        // --- Сохранение и загрузка -----------------------------------------
-        private async System.Threading.Tasks.Task SaveAsync()
-        {
-            var topLevel = GetTopLevel();
-            if (topLevel == null) return;
-
-            var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+            Name = "Y Axis",
+            NamePaint = new SolidColorPaint(SKColors.Red),
+            LabelsPaint = new SolidColorPaint(SKColors.Green),
+            TextSize = 20,
+            SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray)
             {
-                Title = "Сохранить функции",
-                SuggestedFileName = "functions.json",
-                FileTypeChoices = new[] { new FilePickerFileType("JSON") { Patterns = new[] { "*.json" } } }
-            });
-
-            if (file == null) return;
-
-            var json = JsonSerializer.Serialize(Functions);
-            await File.WriteAllTextAsync(file.Path.LocalPath, json);
-            IsDirty = false;
-        }
-
-        private async System.Threading.Tasks.Task LoadAsync()
-        {
-            var topLevel = GetTopLevel();
-            if (topLevel == null) return;
-
-            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
-            {
-                Title = "Открыть функции",
-                AllowMultiple = false,
-                FileTypeFilter = new[] { new FilePickerFileType("JSON") { Patterns = new[] { "*.json" } } }
-            });
-
-            if (files.Count == 0) return;
-
-            var text = await File.ReadAllTextAsync(files[0].Path.LocalPath);
-            var funcs = JsonSerializer.Deserialize<ObservableCollection<FunctionModel>>(text);
-            if (funcs == null) return;
-
-            Functions.Clear();
-            foreach (var f in funcs)
-            {
-                f.RebuildPlot();
-                Functions.Add(f);
-                SubscribeToFunction(f);
+                StrokeThickness = 2,
+                PathEffect = new DashEffect(new float[] { 3, 3 })
             }
-            SelectedFunction = Functions.FirstOrDefault();
-            IsDirty = false;
         }
+    ];
 
-        private TopLevel? GetTopLevel() =>
-            (Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+    public ObservablePoint? SelectedPoint { get; set; }
+    public ICommand AddPointCommand { get; }
+    public ICommand RemovePointCommand { get; }
 
-        // --- Подписки для автообновления графика ---------------------------
-        private void SubscribeToFunction(FunctionModel f)
+    public MainWindowViewModel()
+    {
+        Points = new ObservableCollection<ObservablePoint>
         {
-            f.Nodes.CollectionChanged += (_, __) => { f.RebuildPlot(); IsDirty = true; };
-
-            foreach (var n in f.Nodes)
-                n.PropertyChanged += (_, __) => { f.RebuildPlot(); IsDirty = true; };
-
-            f.Nodes.CollectionChanged += (_, e) =>
+            new(0, 0),
+            new(1, 2),
+            new(2, 1)
+        };
+        Series =
+        [
+            new LineSeries<ObservablePoint>
             {
-                if (e.NewItems != null)
-                    foreach (Node n in e.NewItems)
-                        n.PropertyChanged += (_, __) => { f.RebuildPlot(); IsDirty = true; };
-            };
-        }
+                Values =
+                [
+                    new(0, 0),
+                    new(1, 2),
+                    new(2, 1),
+                    new(-2, -10)
+                ],
+                Name = "Function 1"
+            },
+            new LineSeries<ObservablePoint>
+            {
+                Values =
+                [
+                    new(0, 1),
+                    new(1, 3),
+                    new(2, 2)
+                ],
+                Name = "Function 2"
+            }
+        ];
+        AddPointCommand = ReactiveCommand.Create(AddPoint);
+        RemovePointCommand = ReactiveCommand.Create(RemovePoint);
+    }
 
-        // --- Drag & Drop на графике ----------------------------------------
-        public void OnPlotPointerPressed(Point p, OxyPlot.Avalonia.PlotView pv)
-        {
-            if (SelectedFunction == null) return;
-            var model = pv.ActualModel;
-            if (model == null) return;
+    private void AddPoint()
+    {
+        var nextX = Points.Any() ? Points.Max(p => p.X) + 1 : 0;
+        Points.Add(new ObservablePoint(nextX, 0));
+    }
 
-            var sp = new ScreenPoint(p.X, p.Y);
-            var data = Axis.InverseTransform(sp, model.DefaultXAxis, model.DefaultYAxis);
-
-            var nearest = SelectedFunction.Nodes
-                .OrderBy(n => Math.Pow(n.X - data.X, 2) + Math.Pow(n.Y - data.Y, 2))
-                .FirstOrDefault();
-
-            if (nearest == null) return;
-
-            var dx = nearest.X - data.X;
-            var dy = nearest.Y - data.Y;
-            var dist = Math.Sqrt(dx * dx + dy * dy);
-            if (dist < 0.2) // порог
-                _draggedNode = nearest;
-        }
-
-        public void OnPlotPointerMoved(Point p, OxyPlot.Avalonia.PlotView pv)
-        {
-            if (_draggedNode == null || SelectedFunction == null) return;
-            var model = pv.ActualModel;
-            if (model == null) return;
-
-            var sp = new ScreenPoint(p.X, p.Y);
-            var data = Axis.InverseTransform(sp, model.DefaultXAxis, model.DefaultYAxis);
-            _draggedNode.X = data.X;
-            _draggedNode.Y = data.Y;
-            SelectedFunction.RebuildPlot();
-            IsDirty = true;
-        }
-
-        public void OnPlotPointerReleased()
-        {
-            _draggedNode = null;
-        }
+    private void RemovePoint()
+    {
+        if (SelectedPoint != null)
+            Points.Remove(SelectedPoint);
     }
 }
--- AvaloniaApp/Views/MainWindow.axaml.cs
+++ AvaloniaApp/Views/MainWindow.axaml.cs
@@ -1,7 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Input;
-using AvaloniaApp.ViewModels;
-using OxyPlot.Avalonia;
 
 namespace AvaloniaApp.Views;
 
@@ -10,42 +7,5 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-    }
-    
-    private void PlotView_PointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        var vm = this.DataContext as MainWindowViewModel;
-        if (vm == null) return;
-        var pv = sender as PlotView;
-        if (pv == null) return;
-
-        var p = e.GetCurrentPoint(pv).Position; // Avalonia Point (in device-independent pixels)
-        vm.OnPlotPointerPressed(p, pv);
-        e.Handled = true;
-    }
-
-    private void PlotView_PointerMoved(object? sender, PointerEventArgs e)
-    {
-        var vm = this.DataContext as MainWindowViewModel;
-        if (vm == null) return;
-        var pv = sender as PlotView;
-        if (pv == null) return;
-
-        var p = e.GetCurrentPoint(pv).Position;
-        vm.OnPlotPointerMoved(p, pv);
-        e.Handled = true;
-    }
-
-    private void PlotView_PointerReleased(object? sender, PointerReleasedEventArgs e)
-    {
-        var vm = this.DataContext as MainWindowViewModel;
-        if (vm == null) return;
-        var pv = sender as PlotView;
-        if (pv == null) return;
-
-        var p = e.GetCurrentPoint(pv).Position;
-        // vm.OnPlotPointerReleased(p, pv);
-        vm.OnPlotPointerReleased();
-        e.Handled = true;
     }
 }
